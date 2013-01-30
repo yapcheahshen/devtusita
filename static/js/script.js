c38de043d7ddb5887ed1eee6c33e325372048800
siongui <siongui@gmail.com>
2013-01-30 23:03:31 +0800

    save user data to server

--- static/js/script.js
+++ static/js/script.js
@@ -15,35 +15,65 @@ angular.module('tusitaPersonal', [], function($routeProvider, $locationProvider)
 function mainCtrl($scope, $http, $templateCache, $location) {
   $scope.userEmail = angular.element(document.getElementById('userEmail')).html();
   $scope.urlREST = '/RESTful/' + $scope.userEmail;
+  // check whether user logged in
   $scope.isLogin = (function() {
+    // test whether email is valid
     var patt=/[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}/;
     return patt.test($scope.userEmail);
   })();
 
   if ($scope.isLogin) {
+    // user already logged in, retrieve user data from server.
     $http({method: 'GET', url: $scope.urlREST, cache: $templateCache}).
       success(function(data, status) {
+        // retrieve user data successfully
         $scope.status = status;
         $scope.data = data;
         $scope.isFirstLogin = false;
       }).
       error(function(data, status) {
+        // fail to retrieve user data => user logged in for the first time
         $scope.status = status;
         $scope.data = data || "Request failed";
         $scope.isFirstLogin = true;
+        // redirect user to fill in basic user data.
         $location.path('/firstLogin');
     });
   }
 }
 
-function firstLoginCtrl($scope) {
+function firstLoginCtrl($scope, $http, $templateCache, $location) {
   $scope.email = $scope.userEmail;
   $scope.master = {};
 
+  // callback if user press 'SAVE' button
   $scope.update = function(user) {
+    $scope.savingUserData = true;
     $scope.master= angular.copy(user);
+
+    // save user data to server through RESTful API
+    $http({method: 'POST',
+           url: $scope.urlREST,
+           data: JSON.stringify($scope.master),
+           cache: $templateCache}).
+      success(function(data, status) {
+        // save successfully
+        $scope.savingUserData = undefined;
+        $scope.failToSaveUserData = undefined;
+        $scope.status = status;
+        $scope.data = data;
+        $location.path('/');
+      }).
+      error(function(data, status) {
+        // failed to save user data
+        $scope.savingUserData = undefined;
+        $scope.failToSaveUserData = true;
+        $scope.data = data || "Request failed";
+        $scope.status = status;
+    });
   };
  
+  // callback if user press 'RESET' button
   $scope.reset = function() {
     $scope.user = angular.copy($scope.master);
   };
