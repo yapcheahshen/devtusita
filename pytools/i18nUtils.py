a5746dbc19e2acfcd7421738b7c042e77315bd9f
siongui <siongui@gmail.com>
2013-03-02 22:02:47 +0800

    update i18n utils

--- pytools/i18nUtils.py
+++ pytools/i18nUtils.py
@@ -9,17 +9,14 @@
 # http://www.supernifty.org/blog/2011/09/16/python-localization-made-easy/
 
 import os, sys, re, json, shutil
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), '../gaepalilibs'))
-from jianfan import ftoj
 
 # global variable
 locale_dir = os.path.join(os.path.dirname(__file__), '../locale')
 html_dir = os.path.join(os.path.dirname(__file__), '../app')
 potpath = os.path.join(locale_dir, 'messages.pot')
 twPoPath = os.path.join(locale_dir, 'zh_TW/LC_MESSAGES/messages.po')
-cnPoPath = os.path.join(locale_dir, 'zh_CN/LC_MESSAGES/messages.po')
 dstLocalesJs = os.path.join(os.path.dirname(__file__), '../app/js/locales.js') 
-locales = ['en_US', 'zh_TW', 'zh_CN']
+locales = ['zh_TW']
 
 
 def createPOT():
@@ -78,32 +75,6 @@ def initOrUpdatePOs():
       initLocalePO(locale)
 
 
-def formatMO(locale):
-  popath = os.path.join(locale_dir, '%s/LC_MESSAGES/messages.po' % locale)
-  mopath = popath[:-2] + 'mo'
-
-  cmd_msgfmt = 'msgfmt %s -o %s' % (popath, mopath)
-  print(cmd_msgfmt)
-  os.system(cmd_msgfmt)
-
-
-def POtoMO():
-  for locale in locales:
-    formatMO(locale)
-
-
-def TWtoCN():
-  with open(twPoPath, 'r') as f:
-    with open(cnPoPath, 'w') as fd:
-      for line in f.readlines():
-        if 'zh_TW' in line:
-          fd.write(line.replace('zh_TW', 'zh_CN'))
-        elif line.startswith('msgstr'):
-          fd.write(re.sub('msgstr "(.+)"', lambda m: 'msgstr "%s"' % ftoj(m.group(1)), line).encode('utf-8'))
-        else:
-          fd.write(line)
-
-
 def writeJs():
   # create PO-like js file for i18n
   with open(twPoPath, 'r') as f:
@@ -114,7 +85,7 @@ def writeJs():
     obj['zh_TW'][tuple[0].decode('utf-8')] = tuple[1].decode('utf-8')
 
   with open(dstLocalesJs, 'w') as f:
-    f.write('var paliI18nLocaleStrs = ')
+    f.write('var tusitaLocales = ')
     f.write(json.dumps(obj))
     f.write(';')
 
@@ -139,14 +110,6 @@ if __name__ == '__main__':
 
   if sys.argv[1] == "po":
     initOrUpdatePOs()
-    sys.exit(0)
-
-  if sys.argv[1] == "mo":
-    POtoMO()
-    sys.exit(0)
-
-  if sys.argv[1] == "cn":
-    TWtoCN()
     sys.exit(0)
 
   if sys.argv[1] == "js":
